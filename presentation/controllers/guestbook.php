46690a29184bb431c5a39f4e7523fec53dc4d878
Hayden Waisanen <haydenzone@gmail.com>
2013-04-22 13:58:09 -0600

    Db config

---
+++ presentation/controllers/guestbook.php
@@ -0,0 +1,9 @@
+<?php
+class Guestbook extends CI_Controller {
+
+	public function index()
+	{
+		echo 'Hello World!';
+	}
+}
+?>
---
+++ presentation/controllers/signup.php
@@ -0,0 +1,9 @@
+<?php
+class Signup extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('signup');
+	}
+}
+?>
